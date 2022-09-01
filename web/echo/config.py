ab30896147e1a7154ee1248b7dd256d4575576e0
Sam McKendrick <sam.mckendrick@thoughtworks.com>
2022-09-01 13:03:56 +1000

    fix(web): update flask and werkzeug + remove redundant secret

--- web/echo/config.py
+++ web/echo/config.py
@@ -2,11 +2,11 @@ import os
 
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "changeme"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', False)
-DB_NAME = "postgres"
-DB_USER = "postgres"
-DB_PASS = "postgres"
+DB_NAME = os.environ.get('DB_NAME', "postgres")
+DB_USER = os.environ.get('DB_USER', "postgres")
+DB_PASS = os.environ.get('DB_PASS')
 DB_SERVICE = "postgres"
 DB_PORT = "5432"
 SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
